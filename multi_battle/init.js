878e878d6618c06faf46db632dd3dfccf86dd59f
playerkeisukefall <playerkeisukefall@gmail.com>
2018-05-02 16:19:09 +0900

    bullet 同期 改良

--- multi_battle/init.js
+++ multi_battle/init.js
@@ -84,7 +84,7 @@ function update_bullet(){
   let rm_index;
   for(let i=0; i<len; i++){
     if(0 <= bullet[i].y && bullet[i].y <= window_height)
-      bullet_exist[i] = 1;
+      bullet_exist[i] = bullet[i].b_id;
     else{
       bullet_exist[i] = 0;
       rm_index = i;
--- multi_battle/sync.js
+++ multi_battle/sync.js
@@ -13,7 +13,7 @@ function update_opponent(op_info){
   opponent.x = 800 - op_info.x - 70;
 }
 
-check_op_b_exist(op_b_id){
+function check_op_b_exist(op_b_id){
   for(let i=0; i<op_bullet.length; i++){
     if(op_bullet[i].b_id == op_b_id) return op_bullet[i];
   }
@@ -39,6 +39,7 @@ function update_opponent_bullet(obia, op_b_exist){ // obia: op_bullet_info_arr
       op_b.y = obia[i].y;
     }
     else{ // 存在しない
+      console.log("false!")
       let op_b_len = op_bullet.length;
       op_bullet[op_b_len] = create_sprite({w:16,h:16}, {x:obia[i].x,y:obia[i].y}, obia.image, obia.frame, {x:obia.scale_x,y:obia.scale_y}, undefined, {x:0,y:obia.speed_y}, obia.bul_power, obia.b_id);
       op_bullet[op_b_len].on('enterframe', op_bulet_enterframe);
@@ -71,8 +72,8 @@ function sync_info(){
   socket.on("on_battle_get", function(data){
     update_opponent(data.player_info);
     let op_bullet_info_arr = inverse_pos(data.bullet_info);
+    console.log(data.bullet_exist);
     update_opponent_bullet(op_bullet_info_arr, data.bullet_exist);
-
   })
 
 }
